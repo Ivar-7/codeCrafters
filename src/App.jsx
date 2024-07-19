5f912afafade8931d1020dbdaf0d2f16df9f8ecf
Ivar-7 <leank6071@gmail.com>
2024-07-20 01:06:02 +0300

    working dashboard

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,199 @@
-import React from "react";
-import { DashboardProvider } from "./components/api/FirebaseApi";
-import Navbar from "./components/home/Navbar";
+import React, { useState, useEffect } from "react";
 import "./App.css";
 
-function App() {
+const orders = [
+  {
+    cropName: "Maize",
+    datePlanted: "2-3-2024",
+    growthStage: "Reproductive Stage",
+    condition: "Good",
+  },
+  {
+    cropName: "Wheat",
+    datePlanted: "1-2-2024",
+    growthStage: "Maturation",
+    condition: "Perfect",
+  },
+  {
+    cropName: "Barley",
+    datePlanted: "3-1-2024",
+    growthStage: "Maturation",
+    condition: "Perfect",
+  },
+];
+
+const App = () => {
+  const [isDarkMode, setIsDarkMode] = useState(true);
+
+  useEffect(() => {
+    if (isDarkMode) {
+      document.body.classList.add("dark-mode-variables");
+    } else {
+      document.body.classList.remove("dark-mode-variables");
+    }
+  }, [isDarkMode]);
+
   return (
-    <>
-      <DashboardProvider>
-        <Navbar />
-      </DashboardProvider>
-    </>
+    <body>
+      <div class="container">
+        <aside>
+          <div class="toggle">
+            <div class="logo">
+              <img src="images/logo.png" />
+              <h2>
+                Agro<span class="danger">Intel</span>
+              </h2>
+            </div>
+            <div class="close" id="close-btn">
+              <span class="material-icons-sharp">close</span>
+            </div>
+          </div>
+
+          <div class="sidebar">
+            <a href="#">
+              <span class="material-icons-sharp">dashboard</span>
+              <h3>Dashboard</h3>
+            </a>
+            <a href="#" class="active">
+              <span class="material-icons-sharp">person_outline</span>
+              <h3>Farm Management</h3>
+            </a>
+            <a href="#">
+              <span class="material-icons-sharp">receipt_long</span>
+              <h3>Crop Management</h3>
+            </a>
+            <a href="#">
+              <span class="material-icons-sharp">insights</span>
+              <h3>Camera Assistance</h3>
+            </a>
+            <a href="#">
+              <span class="material-icons-sharp">mail_outline</span>
+              <h3>Analytics & Reports</h3>
+              <span class="message-count">27</span>
+            </a>
+            <a href="#">
+              <span class="material-icons-sharp">inventory</span>
+              <h3>Chat Bot</h3>
+            </a>
+            <a href="#">
+              <span class="material-icons-sharp">report_gmailerrorred</span>
+              <h3>Support</h3>
+            </a>
+            <a href="#">
+              <span class="material-icons-sharp">settings</span>
+              <h3>Settings</h3>
+            </a>
+            <a href="#">
+              <span class="material-icons-sharp">add</span>
+              <h3>About US</h3>
+            </a>
+            <a href="#">
+              <span class="material-icons-sharp">logout</span>
+              <h3>Logout</h3>
+            </a>
+          </div>
+        </aside>
+        <main>
+          <h1>Analytics</h1>
+          <div class="analyse">
+            <div class="sales">
+              <div class="status">
+                <div class="info">
+                  <h3>Farm Efficiency</h3>
+                  <h1>Good</h1>
+                </div>
+                <div class="progresss">
+                  <svg>
+                    <circle cx="38" cy="38" r="36"></circle>
+                  </svg>
+                  <div class="percentage">
+                    <p>+81%</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="visits">
+              <div class="status">
+                <div class="info">
+                  <h3>Resource Utilization</h3>
+                  <h1>Above Average</h1>
+                </div>
+                <div class="progresss">
+                  <svg>
+                    <circle cx="38" cy="38" r="36"></circle>
+                  </svg>
+                  <div class="percentage">
+                    <p>68%</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="searches">
+              <div class="status">
+                <div class="info">
+                  <h3>Cost Savings</h3>
+                  <h1>$4,147</h1>
+                </div>
+                <div class="progresss">
+                  <svg>
+                    <circle cx="38" cy="38" r="36"></circle>
+                  </svg>
+                  <div class="percentage">
+                    <p>+21%</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="new-users">
+            <h2>Farm Management</h2>
+            <div class="user-list">
+              <div class="user">
+                <img src="images/profile-4.jpeg" />
+                <h2>Soil Health</h2>
+                <p>
+                  Last Checked <b>54 Min Ago</b>
+                </p>
+              </div>
+              <div class="user">
+                <img src="images/profile-3.jpeg" />
+                <h2>Climate Data</h2>
+                <p>
+                  Updated <b>3 Hours Ago</b>
+                </p>
+              </div>
+              <div class="user">
+                <img src="images/profile-2.jpeg" />
+                <h2>Crop Plan</h2>
+                <p>reviewed 6 Hours Ago</p>
+              </div>
+              <div class="user">
+                <img src="images/plus.png" />
+                <h2>More</h2>
+                <p>New Info</p>
+              </div>
+            </div>
+          </div>
+          <div class="crop-management">
+            <h2>Crop Management</h2>
+            <table>
+              <thead>
+                <tr>
+                  <th>Crop Name</th>
+                  <th>Date Planted</th>
+                  <th>Growth Stage Today</th>
+                  <th>Crop Condition</th>
+                  <th></th>
+                </tr>
+              </thead>
+              <tbody></tbody>
+            </table>
+            <a href="#">Show All</a>
+          </div>
+        </main>
+      </div>
+    </body>
   );
-}
+};
 
 export default App;
