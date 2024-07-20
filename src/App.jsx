9a4be4c6bcd76913fd5b68602649bef254a1d750
Ivar-7 <leank6071@gmail.com>
2024-07-20 12:08:18 +0300

    refactor

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,11 @@
 import React, { useState } from "react";
 import "./App.css";
 import Navbar from "./components/home/Navbar";
-import Dashboard from "./components/home/Dashboard";
 
 const App = () => {
-  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
-
-  const toogleSidebar = () => setIsSidebarOpen(!isSidebarOpen);
-
   return (
     <div className="container">
-      <Navbar isSidebarOpen={isSidebarOpen} />
-      <Dashboard toogleSidebar={toogleSidebar} />
+      <Navbar />
     </div>
   );
 };
--- src/components/home/Navbar.jsx
+++ src/components/home/Navbar.jsx
@@ -1,64 +1,71 @@
-import React from "react";
+import React, { useState } from "react";
+import Dashboard from "./Dashboard";
 
-const Navbar = ({ isSidebarOpen }) => {
+const Navbar = () => {
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+
+  const toogleSidebar = () => setIsSidebarOpen(!isSidebarOpen);
   return (
-    <aside style={{ display: isSidebarOpen ? "block" : "" }}>
-      <div className="toggle">
-        <div className="logo">
-          <img src="images/logo.png" />
-          <h2>
-            Agro<span className="danger">Intel</span>
-          </h2>
+    <>
+      <aside style={{ display: isSidebarOpen ? "block" : "" }}>
+        <div className="toggle">
+          <div className="logo">
+            <img src="images/logo.png" />
+            <h2>
+              Agro<span className="danger">Intel</span>
+            </h2>
+          </div>
+          <div className="close" id="close-btn">
+            <span className="material-icons-sharp">close</span>
+          </div>
         </div>
-        <div className="close" id="close-btn">
-          <span className="material-icons-sharp">close</span>
-        </div>
-      </div>
 
-      <div className="sidebar">
-        <a href="/">
-          <span className="material-icons-sharp">dashboard</span>
-          <h3>Dashboard</h3>
-        </a>
-        <a href="/farm" className="active">
-          <span className="material-icons-sharp">person_outline</span>
-          <h3>Farm Management</h3>
-        </a>
-        <a href="#">
-          <span className="material-icons-sharp">receipt_long</span>
-          <h3>Crop Management</h3>
-        </a>
-        <a href="#">
-          <span className="material-icons-sharp">insights</span>
-          <h3>Camera Assistance</h3>
-        </a>
-        <a href="#">
-          <span className="material-icons-sharp">mail_outline</span>
-          <h3>Analytics & Reports</h3>
-          <span className="message-count">27</span>
-        </a>
-        <a href="#">
-          <span className="material-icons-sharp">inventory</span>
-          <h3>Chat Bot</h3>
-        </a>
-        <a href="#">
-          <span className="material-icons-sharp">report_gmailerrorred</span>
-          <h3>Support</h3>
-        </a>
-        <a href="#">
-          <span className="material-icons-sharp">settings</span>
-          <h3>Settings</h3>
-        </a>
-        <a href="#">
-          <span className="material-icons-sharp">add</span>
-          <h3>About US</h3>
-        </a>
-        <a href="#">
-          <span className="material-icons-sharp">logout</span>
-          <h3>Logout</h3>
-        </a>
-      </div>
-    </aside>
+        <div className="sidebar">
+          <a href="/">
+            <span className="material-icons-sharp">dashboard</span>
+            <h3>Dashboard</h3>
+          </a>
+          <a href="/farm" className="active">
+            <span className="material-icons-sharp">person_outline</span>
+            <h3>Farm Management</h3>
+          </a>
+          <a href="#">
+            <span className="material-icons-sharp">receipt_long</span>
+            <h3>Crop Management</h3>
+          </a>
+          <a href="#">
+            <span className="material-icons-sharp">insights</span>
+            <h3>Camera Assistance</h3>
+          </a>
+          <a href="#">
+            <span className="material-icons-sharp">mail_outline</span>
+            <h3>Analytics & Reports</h3>
+            <span className="message-count">27</span>
+          </a>
+          <a href="#">
+            <span className="material-icons-sharp">inventory</span>
+            <h3>Chat Bot</h3>
+          </a>
+          <a href="#">
+            <span className="material-icons-sharp">report_gmailerrorred</span>
+            <h3>Support</h3>
+          </a>
+          <a href="#">
+            <span className="material-icons-sharp">settings</span>
+            <h3>Settings</h3>
+          </a>
+          <a href="#">
+            <span className="material-icons-sharp">add</span>
+            <h3>About US</h3>
+          </a>
+          <a href="#">
+            <span className="material-icons-sharp">logout</span>
+            <h3>Logout</h3>
+          </a>
+        </div>
+      </aside>
+      <Dashboard toogleSidebar={toogleSidebar} />
+    </>
   );
 };
 
