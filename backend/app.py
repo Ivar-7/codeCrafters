a30432b12c31cb432c009d01f0bf46cfb9598d9f
Kevin Mwangi <127428309+Ivar-7@users.noreply.github.com>
2024-07-21 16:45:21 +0300

    backend hosted (#7)
    * change to python3
    
    * change port
    
    * use gunicorn to run server

--- backend/app.py
+++ backend/app.py
@@ -30,4 +30,5 @@ def api():
         return jsonify({"error": str(e)}), 500
 
 if __name__ == '__main__':
-    app.run()
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
